a0684e6a2c37b9d2a8c0107d712e9ddda46a27e5
Tyler Zon <tylerzon@yahoo.com>
2022-08-16 14:22:47 -0400

    Updating plotly chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,7 +79,7 @@ styler = my_hours_list.style.format(
 
 #st.dataframe(my_hours_list)
 
-fig = px.line(my_hours_list, x="Week", y="Meeting Hours", color="Meeting Hours")
+fig = px.line(my_hours_list, x="Week", y="Meeting Hours")
 st.plotly_chart(fig)
 my_meetings_list = pd.read_csv("Meeting_Numbers.csv")
 
